b70ad709a4348a37bda1477d521031d6821fa392
gonella <gonella@gmail.com>
2015-09-20 15:48:33 -0300

    Change layout + Added images
    Change-Id: Ia5e973bf25f341186f092ab557a499c6178bf267

--- TwoCents/com.twocents.ui/src/main/java/com/twocents/ui/client/components/TwLogin.java
+++ TwoCents/com.twocents.ui/src/main/java/com/twocents/ui/client/components/TwLogin.java
@@ -5,16 +5,17 @@ import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
-import swing2swt.layout.BorderLayout;
-
 import com.swtdesigner.SWTResourceManager;
 import com.twocents.ui.constant.UIDefault;
+
+import swing2swt.layout.BorderLayout;
 
 /**
  * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
@@ -28,128 +29,156 @@ import com.twocents.ui.constant.UIDefault;
  */
 public class TwLogin extends Composite {
 
-	public Text getUserTxt() {
-		return userTxt;
-	}
+    public Text getUserTxt() {
+        return userTxt;
+    }
 
-	public Text getPwdTxt() {
-		return pwdTxt;
-	}
+    public Text getPwdTxt() {
+        return pwdTxt;
+    }
 
-	private Text userTxt;
-	private Text pwdTxt;
-	private Button moreStockBroker;
-	private Button button1;
-	private Button loginBtn;
+    private Text userTxt;
+    private Text pwdTxt;
+    private Button moreStockBroker;
+    private Button button1;
+    private Button loginBtn;
 
-	/**
-	 * Create the composite.
-	 * 
-	 * @param parent
-	 * @param style
-	 */
-	public TwLogin(Composite parent, int style) {
-		super(parent, style);
-		setLayout(new BorderLayout(0, 0));
+    /**
+     * Create the composite.
+     *
+     * @param parent
+     * @param style
+     */
+    public TwLogin(Composite parent, int style) {
+        super(parent, style);
+        setLayout(new BorderLayout(0, 0));
 
-		Canvas canvas = new Canvas(this, SWT.NONE);
-		canvas.setBackgroundMode(SWT.INHERIT_DEFAULT);
-		canvas.setLayoutData(BorderLayout.CENTER);
-		canvas.setBackgroundImage(SWTResourceManager.getImage(TwLogin.class,
-				UIDefault.COM_TWOCENTS_UI_RESOURCES_IMAGES_LOGIN_SCREEN_PNG));
-		Canvas canvas_1 = new Canvas(canvas, SWT.NONE);
+        Canvas canvas = new Canvas(this, SWT.NONE);
+        canvas.setBackgroundMode(SWT.INHERIT_DEFAULT);
+        canvas.setLayoutData(BorderLayout.CENTER);
+        canvas.setBackgroundImage(SWTResourceManager.getImage(TwLogin.class,
+                UIDefault.COM_TWOCENTS_UI_RESOURCES_IMAGES_LOGIN_SCREEN_PNG));
+        canvas.setLayout(new GridLayout(6, false));
 
-		int x = (getDisplay().getClientArea().width - 300) / 2;
-		int y = (getDisplay().getClientArea().height - 500) / 2;
-		canvas_1.setBounds(x, y, 272, 144);
+        int x = (getDisplay().getClientArea().width - 300) / 2;
+        int y = (getDisplay().getClientArea().height - 500) / 2;
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        new Label(canvas, SWT.NONE);
+        Canvas canvas_1 = new Canvas(canvas, SWT.NONE);
 
-		Label Login = new Label(canvas_1, SWT.NONE);
-		Login.setFont(SWTResourceManager.getFont("Tahoma", 8, SWT.BOLD));
-		Login.setBounds(31, 22, 49, 13);
-		Login.setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
-		Login.setText("Usu\u00E1rio:");
+        Label Login = new Label(canvas_1, SWT.NONE);
+        Login.setFont(SWTResourceManager.getFont("Tahoma", 8, SWT.BOLD));
+        Login.setBounds(31, 22, 49, 13);
+        Login.setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
+        Login.setText("Usu\u00E1rio:");
 
-		setUserTxt(new Text(canvas_1, SWT.BORDER));
-		getUserTxt().setBounds(86, 22, 174, 19);
+        setUserTxt(new Text(canvas_1, SWT.BORDER));
+        getUserTxt().setBounds(86, 22, 174, 19);
 
-		getUserTxt().addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent event) {
-				switch (event.keyCode) {
-				case SWT.CR:
-					logar(null);
-					break;
-				}
-			}
-		});
+        getUserTxt().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent event) {
+                switch (event.keyCode) {
+                    case SWT.CR:
+                        logar(null);
+                        break;
+                }
+            }
+        });
 
-		Label lblSenha = new Label(canvas_1, SWT.NONE);
-		lblSenha.setFont(SWTResourceManager.getFont("Tahoma", 8, SWT.BOLD));
-		lblSenha.setBounds(31, 56, 49, 13);
-		lblSenha.setText("Senha:");
-		lblSenha.setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
-		pwdTxt = new Text(canvas_1, SWT.BORDER | SWT.PASSWORD);
-		pwdTxt.setBounds(86, 50, 174, 19);
+        Label lblSenha = new Label(canvas_1, SWT.NONE);
+        lblSenha.setFont(SWTResourceManager.getFont("Tahoma", 8, SWT.BOLD));
+        lblSenha.setBounds(31, 56, 49, 13);
+        lblSenha.setText("Senha:");
+        lblSenha.setForeground(SWTResourceManager.getColor(SWT.COLOR_GRAY));
+        pwdTxt = new Text(canvas_1, SWT.BORDER | SWT.PASSWORD);
+        pwdTxt.setBounds(86, 50, 174, 19);
 
-		pwdTxt.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent event) {
-				switch (event.keyCode) {
-				case SWT.CR:
-					logar(null);
-					break;
-				}
-			}
-		});
+        pwdTxt.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent event) {
+                switch (event.keyCode) {
+                    case SWT.CR:
+                        logar(null);
+                        break;
+                }
+            }
+        });
 
-		loginBtn = new Button(canvas_1, SWT.NONE);
-		loginBtn.addMouseListener(new MouseAdapter() {
-			public void mouseDown(MouseEvent arg0) {
-				logar(arg0);
-			}
-		});
-		loginBtn.setBounds(106, 107, 75, 25);
-		loginBtn.setText("Logar");
+        loginBtn = new Button(canvas_1, SWT.NONE);
+        loginBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseDown(MouseEvent arg0) {
+                logar(arg0);
+            }
+        });
+        loginBtn.setBounds(106, 107, 75, 25);
+        loginBtn.setText("Logar");
 
-		Button pwdBtn = new Button(canvas_1, SWT.NONE);
-		pwdBtn.setBounds(187, 107, 75, 25);
-		pwdBtn.setText("Cancelar");
-		pwdBtn.addMouseListener(new MouseAdapter() {
-			public void mouseDown(MouseEvent arg0) {
-				getDisplay().close();
-				dispose();
-			}
-		});
+        Button pwdBtn = new Button(canvas_1, SWT.NONE);
+        pwdBtn.setBounds(187, 107, 75, 25);
+        pwdBtn.setText("Cancelar");
+        pwdBtn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseDown(MouseEvent arg0) {
+                getDisplay().close();
+                dispose();
+            }
+        });
 
-		button1 = new Button(canvas_1, SWT.CHECK | SWT.LEFT);
-		button1.setText("Salvar credenciais");
-		button1.setBounds(86, 71, 116, 30);
+        button1 = new Button(canvas_1, SWT.CHECK | SWT.LEFT);
+        button1.setText("Salvar credenciais");
+        button1.setBounds(86, 71, 116, 30);
 
-		moreStockBroker = new Button(canvas_1, SWT.PUSH | SWT.CENTER);
-		moreStockBroker.setText("+");
-		moreStockBroker.setBounds(31, 107, 32, 25);
-		moreStockBroker.setToolTipText("Criar um novo corretor");
-		moreStockBroker.addMouseListener(new MouseAdapter() {
-			public void mouseDown(MouseEvent arg0) {
-				createStockBroker(arg0);
-			}
-		});
+        moreStockBroker = new Button(canvas_1, SWT.PUSH | SWT.CENTER);
+        moreStockBroker.setText("+");
+        moreStockBroker.setBounds(31, 107, 32, 25);
+        moreStockBroker.setToolTipText("Criar um novo corretor");
+        moreStockBroker.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseDown(MouseEvent arg0) {
+                createStockBroker(arg0);
+            }
+        });
 
-		//getUserTxt().setText("gonella");
+        //getUserTxt().setText("gonella");
 
-	}
+    }
 
-	@Override
-	protected void checkSubclass() {
-		// Disable the check that prevents subclassing of SWT components
-	}
+    @Override
+    protected void checkSubclass() {
+        // Disable the check that prevents subclassing of SWT components
+    }
 
-	public void logar(MouseEvent event) {
-	}
+    public void logar(MouseEvent event) {
+    }
 
-	public void createStockBroker(MouseEvent event) {
-	}
+    public void createStockBroker(MouseEvent event) {
+    }
 
-	public void setUserTxt(Text userTxt) {
-		this.userTxt = userTxt;
-	}
+    public void setUserTxt(Text userTxt) {
+        this.userTxt = userTxt;
+    }
 
 }
--- TwoCents/com.twocents.ui/src/main/java/com/twocents/ui/client/components/wizard/TwWizard.java
+++ TwoCents/com.twocents.ui/src/main/java/com/twocents/ui/client/components/wizard/TwWizard.java
@@ -9,18 +9,29 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import com.swtdesigner.SWTResourceManager;
 import com.twocents.context.ContextHolder;
+import com.twocents.core.populate.PopulateData;
+import com.twocents.core.util.StringCheck;
+import com.twocents.exceptions.CoreException;
+import com.twocents.model.Broker;
+import com.twocents.model.FixedTax;
 import com.twocents.model.StockBroker;
+import com.twocents.model.TwUser;
+import com.twocents.ui.client.components.facade.AccountFacade;
+import com.twocents.ui.client.session.ContextHolderUI;
 import com.twocents.ui.constant.UIDefault;
+import com.twocents.ui.exceptions.UIException;
 import com.twocents.ui.util.DialogUtil;
 
 public class TwWizard extends Dialog {
@@ -30,17 +41,19 @@ public class TwWizard extends Dialog {
     private Text emailTxt;
     private Text username;
     private Text passwd;
-    private Label lblPasso;
 
     private NumberFormat currencyFormatter;
     private NumberFormat numberFormatter;
     private Shell dialogShell;
     private CloseShellListener closeListener;
+    private Text txtCorretora;
+    private Text txtFixa;
 
     public void open() {
 
         final Shell parent = getParent();
-        dialogShell = new Shell(parent, SWT.CLOSE | SWT.TITLE
+        dialogShell = new Shell(parent, SWT.CLOSE
+                | SWT.TITLE
                 | SWT.APPLICATION_MODAL);
 
         com.cloudgarden.resource.SWTResourceManager
@@ -52,7 +65,7 @@ public class TwWizard extends Dialog {
         dialogShell.layout();
         dialogShell.pack();
         dialogShell.setText("TwoCents Wizard");
-        dialogShell.setSize(557, 596);
+        dialogShell.setSize(394, 596);
         dialogShell.setLocation(300, 200);
 
         dialogShell.setLocation(getParent().toDisplay(100, 100));
@@ -72,7 +85,74 @@ public class TwWizard extends Dialog {
         dialogShell.setBackgroundMode(SWT.INHERIT_DEFAULT);
 
         createWizardNoticeScreen(dialogShell);
-        //createBrokerComponents(dialogShell);
+
+        Label lblNewLabel_3 = new Label(dialogShell, SWT.NONE);
+        lblNewLabel_3.setFont(org.eclipse.wb.swt.SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
+        lblNewLabel_3.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        lblNewLabel_3.setText("Informa\u00E7\u00F5es sobre corretora :");
+
+        Group groupBroker = new Group(dialogShell, SWT.NONE);
+        groupBroker.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        groupBroker.setLayout(new GridLayout(2, false));
+        groupBroker.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+
+        Label lblNewLabel_1 = new Label(groupBroker, SWT.NONE);
+        lblNewLabel_1.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        lblNewLabel_1.setFont(org.eclipse.wb.swt.SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
+        lblNewLabel_1.setText("Nome :");
+
+        txtCorretora = new Text(groupBroker, SWT.BORDER);
+        txtCorretora.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+        Label lblNewLabel_2 = new Label(groupBroker, SWT.NONE);
+        lblNewLabel_2.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        lblNewLabel_2.setFont(org.eclipse.wb.swt.SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
+        lblNewLabel_2.setText("Corretagem :");
+
+        txtFixa = new Text(groupBroker, SWT.BORDER);
+        txtFixa.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        new Label(dialogShell, SWT.NONE);
+
+        final Button nextBtn = new Button(dialogShell, SWT.NONE);
+        nextBtn.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+        nextBtn.setText("Cadastrar");
+
+        nextBtn.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(final SelectionEvent arg0) {
+
+                try {
+
+                    fillAndRegisterStockBroker();
+
+                    Broker broker = new Broker();
+
+                    StockBroker stockBrokeradded=fillAndRegisterBroker(broker);
+
+                    TwUser user = new PopulateData().populateUser(
+                            stockBrokeradded
+                            .getName(),
+                            stockBrokeradded.getPassword(),
+                            "",
+                            stockBrokeradded
+                            .getEmail(),
+                            stockBrokeradded.getTelefone());
+
+                    new AccountFacade().addAccount(user, stockBrokeradded, broker
+                            .getName(), "1");
+
+                    dialogShell.removeListener(SWT.Close, closeListener);
+                    dialogShell.close();
+
+                } catch (Exception e) {
+                    DialogUtil.errorMessage(dialogShell, e.getMessage(), e
+                            .getMessage());
+                }
+
+            }
+
+
+        });
 
         dialogShell.open();
         Display display = dialogShell.getDisplay();
@@ -83,166 +163,118 @@ public class TwWizard extends Dialog {
 
     }
 
-    private void createWizardNoticeScreen(final Shell dialogShell) {
-        final Composite group = new Composite(dialogShell, SWT.NONE);
-        group.setBounds(10, 82, 531, 344);
+    private StockBroker fillAndRegisterBroker(Broker broker) throws CoreException {
+        if (StringCheck.isEmpty(txtCorretora.getText())) {
+            throw new UIException(9300);
+        }
 
-        Label lblParabnsEObrigado = new Label(group, SWT.CENTER);
-        lblParabnsEObrigado.setAlignment(SWT.LEFT);
-        lblParabnsEObrigado
-        .setText("Parab\u00E9ns e obrigado por utilizar o TwoCents!");
-        lblParabnsEObrigado.setForeground(SWTResourceManager
-                .getColor(SWT.COLOR_WHITE));
-        lblParabnsEObrigado.setFont(SWTResourceManager.getFont("Tahoma", 11,
-                SWT.NORMAL));
-        lblParabnsEObrigado.setBounds(10, 10, 499, 27);
+        StockBroker logged = ContextHolderUI.getStockBrokerLogged();
+        StockBroker added = action.addStockBroker(logged);
+        ContextHolderUI.setStockBrokerSelected(added);
 
-        Label lblASeguirSero = new Label(group, SWT.CENTER);
-        lblASeguirSero
-        .setText("A seguir ser\u00E3o solicitados a cria\u00E7\u00E3o de:");
-        lblASeguirSero.setForeground(SWTResourceManager
-                .getColor(SWT.COLOR_WHITE));
-        lblASeguirSero.setFont(SWTResourceManager.getFont("Tahoma", 11,
-                SWT.NORMAL));
-        lblASeguirSero.setAlignment(SWT.LEFT);
-        lblASeguirSero.setBounds(10, 43, 499, 27);
+        broker.setStockBroker(added);
 
-        Label lblUmaConta = new Label(group, SWT.CENTER);
-        lblUmaConta.setText("1- Uma conta padr\u00E3o de usu\u00E1rio");
-        lblUmaConta.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-        lblUmaConta.setFont(SWTResourceManager.getFont("Tahoma", 12, SWT.BOLD));
-        lblUmaConta.setAlignment(SWT.LEFT);
-        lblUmaConta.setBounds(32, 83, 499, 48);
+        /* Set<Broker> brokers = added.getBrokers();
 
-        Label lblUmaCorretora = new Label(group, SWT.CENTER);
-        lblUmaCorretora.setText("2- Uma corretora de valores");
-        lblUmaCorretora.setForeground(SWTResourceManager
-                .getColor(SWT.COLOR_WHITE));
-        lblUmaCorretora.setFont(SWTResourceManager.getFont("Tahoma", 12,
-                SWT.BOLD));
-        lblUmaCorretora.setAlignment(SWT.LEFT);
-        lblUmaCorretora.setBounds(32, 182, 499, 51);
+        if (logged.getBrokers() == null) {
+            brokers = new TreeSet<Broker>();
+        }
 
-        Label lblEstaContaSer = new Label(group, SWT.WRAP | SWT.CENTER);
-        lblEstaContaSer
-        .setText("Esta conta ser\u00E1 utilizada para exibi\u00E7\u00E3o e monitoramento de suas opera\u00E7\u00F5es na bolsa de valores");
-        lblEstaContaSer.setForeground(SWTResourceManager
-                .getColor(SWT.COLOR_WHITE));
-        lblEstaContaSer.setFont(SWTResourceManager.getFont("Tahoma", 11,
-                SWT.NORMAL));
-        lblEstaContaSer.setAlignment(SWT.LEFT);
-        lblEstaContaSer.setBounds(42, 137, 428, 35);
+        if (!brokers.contains(broker)) {
+            brokers.add(broker);
+        }*/
 
-        Label lblNaCorretoraSer = new Label(group, SWT.WRAP | SWT.CENTER);
-        lblNaCorretoraSer
-        .setText("Na corretora ser\u00E1 definida a corretagem a ser paga por opera\u00E7\u00E3o");
-        lblNaCorretoraSer.setForeground(SWTResourceManager
-                .getColor(SWT.COLOR_WHITE));
-        lblNaCorretoraSer.setFont(SWTResourceManager.getFont("Tahoma", 11,
-                SWT.NORMAL));
-        lblNaCorretoraSer.setAlignment(SWT.LEFT);
-        lblNaCorretoraSer.setBounds(42, 233, 428, 35);
+        broker.setName(txtCorretora.getText());
 
-        lblPasso = new Label(dialogShell, SWT.CENTER);
-        lblPasso.setFont(SWTResourceManager.getFont("Tahoma", 13, SWT.NORMAL));
-        lblPasso.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-        lblPasso.setBounds(27, 10, 499, 66);
-        lblPasso
-        .setText("Configura\u00E7\u00E3o de Primeiro Acceso ao TwoCents!");
+        String valueFixed = txtFixa.getText();
 
-        final Button nextBtn = new Button(dialogShell, SWT.NONE);
-        nextBtn.setBounds(434, 507, 107, 33);
-        nextBtn.setText("Pr\u00F3ximo");
+        if (StringCheck.isEmpty(valueFixed)) {
+            throw new UIException(9304);
+        }
+        FixedTax tax = new FixedTax();
+        tax.setFixed(true);
+        try {
+            tax.setValue(numberFormatter.parse(
+                    valueFixed.trim()).doubleValue());
+        } catch (Exception e) {
+            DialogUtil
+            .errorMessage(
+                    dialogShell,
+                    "Dados Inválidos",
+                    "O formato dos valores está incorreto. Favor corrigir e tentar novamente.");
+            return null;
+        }
+        broker.setTax(tax);
 
-        nextBtn.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(final SelectionEvent arg0) {
+        action.addBroker(broker);
 
-                try {
-                    nextBtn.setVisible(false);
-                    group.setVisible(false);
-                    createAddAccount(dialogShell);
-                } catch (Exception e) {
-                    DialogUtil.errorMessage(group, e.getMessage(), e
-                            .getMessage());
-                }
-
-            }
-        });
+        return added;
     }
 
-    private void createAddAccount(final Shell dialogShell) {
-        final Composite group = new Composite(dialogShell, SWT.NONE);
-        group.setBounds(10, 111, 531, 194);
+    private void fillAndRegisterStockBroker() {
+        StockBroker stb = new StockBroker();
+        stb.setName(nomeTxt.getText());
+        stb.setEmail(emailTxt.getText());
+        stb.setUsername(username.getText());
+        stb.setPassword(passwd.getText());
+        ContextHolder.setStockBrokerSelected(stb);
+    }
 
-        lblPasso.setText("Passo 1 - Adicionar Conta Padr\u00E3o");
+    private void createWizardNoticeScreen(final Shell dialogShell) {
+        dialogShell.setLayout(new GridLayout(1, false));
+
+        Label lblNewLabel = new Label(dialogShell, SWT.NONE);
+        lblNewLabel.setFont(org.eclipse.wb.swt.SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
+        lblNewLabel.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        lblNewLabel.setText("TwoCents Cadastro");
+
+        Label lblNewLabel_4 = new Label(dialogShell, SWT.NONE);
+        lblNewLabel_4.setFont(org.eclipse.wb.swt.SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
+        lblNewLabel_4.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        lblNewLabel_4.setText("Informa\u00E7\u00F5es sobre usu\u00E1rio :");
+
+        Group group = new Group(dialogShell, SWT.NONE);
+        group.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        group.setLayout(new GridLayout(2, false));
+        group.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+
 
         Label nomeLbl = new Label(group, SWT.NONE);
-        nomeLbl.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-        nomeLbl.setBounds(24, 34, 55, 15);
-        nomeLbl.setText("Nome");
-
-        Label lblEmail = new Label(group, SWT.NONE);
-        lblEmail.setText("E-mail");
-        lblEmail.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-        lblEmail.setBounds(24, 76, 55, 15);
-
-        Label lblLogin = new Label(group, SWT.NONE);
-        lblLogin.setText("Login");
-        lblLogin.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-        lblLogin.setBounds(24, 116, 55, 15);
-
-        Label lblSenha = new Label(group, SWT.NONE);
-        lblSenha.setText("Senha");
-        lblSenha.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-        lblSenha.setBounds(24, 151, 55, 15);
+        nomeLbl.setFont(org.eclipse.wb.swt.SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
+        nomeLbl.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        nomeLbl.setText("Nome :");
 
         nomeTxt = new Text(group, SWT.BORDER);
-        nomeTxt.setBounds(87, 34, 208, 21);
+        GridData gd_nomeTxt = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+        gd_nomeTxt.widthHint = 244;
+        nomeTxt.setLayoutData(gd_nomeTxt);
+
+        Label lblEmail = new Label(group, SWT.NONE);
+        lblEmail.setFont(org.eclipse.wb.swt.SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
+        lblEmail.setText("E-mail :");
+        lblEmail.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
 
         emailTxt = new Text(group, SWT.BORDER);
-        emailTxt.setBounds(87, 73, 208, 21);
+        GridData gd_emailTxt = new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1);
+        gd_emailTxt.widthHint = 236;
+        emailTxt.setLayoutData(gd_emailTxt);
+
+        Label lblLogin = new Label(group, SWT.NONE);
+        lblLogin.setFont(org.eclipse.wb.swt.SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
+        lblLogin.setText("Login :");
+        lblLogin.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
 
         username = new Text(group, SWT.BORDER);
-        username.setBounds(85, 113, 120, 21);
+        username.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+
+        Label lblSenha = new Label(group, SWT.NONE);
+        lblSenha.setFont(org.eclipse.wb.swt.SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
+        lblSenha.setText("Senha :");
+        lblSenha.setForeground(org.eclipse.wb.swt.SWTResourceManager.getColor(SWT.COLOR_WHITE));
 
         passwd = new Text(group, SWT.BORDER | SWT.PASSWORD);
-        passwd.setBounds(85, 148, 120, 21);
-
-        final Button nextBtn = new Button(dialogShell, SWT.NONE);
-        nextBtn.setBounds(451, 522, 75, 25);
-        nextBtn.setText("Pr\u00F3ximo");
-
-        nextBtn.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(final SelectionEvent arg0) {
-
-                try {
-
-                    StockBroker stb = new StockBroker();
-                    stb.setName(nomeTxt.getText());
-                    stb.setEmail(emailTxt.getText());
-                    stb.setUsername(username.getText());
-                    stb.setPassword(passwd.getText());
-
-                    group.setVisible(false);
-                    nextBtn.setVisible(false);
-
-                    ContextHolder.setStockBrokerSelected(stb);
-
-                    //createBrokerComponents(dialogShell);
-
-                    TwAddBrokerInfo twAddBroker=new TwAddBrokerInfo(dialogShell, SWT.NONE,action,currencyFormatter,numberFormatter,lblPasso,closeListener);
-
-                } catch (Exception e) {
-                    DialogUtil.errorMessage(group, e.getMessage(), e
-                            .getMessage());
-                }
-
-            }
-        });
+        passwd.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
     }
-
 
     public TwWizard(final Shell parent) {
         super(parent);
@@ -258,6 +290,4 @@ public class TwWizard extends Dialog {
         ((DecimalFormat) numberFormatter).setParseBigDecimal(true);
 
     }
-
-
 }
